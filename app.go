5125969d61b11f657f9f4be0261a664bbb48b270
Chris Ha <chunghha@users.noreply.github.com>
2017-11-07 22:50:27 -0600

    feat: Initial /upload

--- app.go
+++ app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -19,6 +20,21 @@ func main() {
 		echo := c.Param("echo")
 		c.JSON(http.StatusOK, gin.H{
 			"echo": echo,
+		})
+	})
+
+	r.POST("/upload", func(c *gin.Context) {
+		form, _ := c.MultipartForm()
+		files := form.File["upload[]"]
+
+		for _, file := range files {
+			log.Println(file.Filename)
+
+			// Upload the file to specific dst.
+			// c.SaveUploadedFile(file, dst)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"uploaded": len(files),
 		})
 	})
 
